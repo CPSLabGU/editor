ba42b4ff1e4856d52482289520af67b7c48ef609
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2024-02-17 14:02:32 +1000

    Remove event handlers.

--- src/views/Positionable.tsx
+++ src/views/Positionable.tsx
@@ -4,33 +4,34 @@ import Point2D from '../models/Point2D';
 function Positionable({position, setPosition, children}) {
   const [isDragging, setIsDragging] = useState(false);
   const [mousePosition, setMousePosition] = useState(position);
+  const currentPosition = new Point2D(position.x, position.y);
+  const updatePosition = (e) => {
+    if (!isDragging) return;
+    currentPosition.x += e.movementX;
+    currentPosition.y += e.movementY;
+    setMousePosition(new Point2D(currentPosition.x, currentPosition.y));
+  };
+  const endDrag = (e) => {
+    setIsDragging(false);
+    setPosition(new Point2D(currentPosition.x, currentPosition.y));
+  };
   useEffect(() => {
-    console.log("useEffect called!");
     if (isDragging) {
-      const currentPosition = new Point2D(position.x, position.y);
-      window.addEventListener('mousemove', (e) => {
-        currentPosition.x += e.movementX;
-        currentPosition.y += e.movementY;
-        setMousePosition(new Point2D(currentPosition.x, currentPosition.y));
-      });
-      window.addEventListener('mouseup', () => {
-        setIsDragging(false);
-        setPosition(new Point2D(currentPosition.x, currentPosition.y))
-      });
+      window.addEventListener('mousemove', updatePosition);
+      window.addEventListener('mouseup', endDrag);
     } else {
-      window.removeEventListener('mousemove', () => {});
-      window.removeEventListener('mouseup', () => {});
+      window.removeEventListener('mousemove', updatePosition);
+      window.removeEventListener('mouseup', endDrag);
     }
     return () => {
-      window.removeEventListener('mousemove', () => {});
-      window.removeEventListener('mouseup', () => {});
+      window.removeEventListener('mousemove', updatePosition);
+      window.removeEventListener('mouseup', endDrag);
     };
   }, [position, isDragging, setMousePosition, setPosition]);
   const mouseDown = useCallback((e: MouseEvent) => {
     e.preventDefault();
+    e.stopPropagation();
     setIsDragging(true);
-    console.log(e);
-    console.log("Drag start!");
   }, [setIsDragging]);
   const positionStyle = {
     left: isDragging ? mousePosition.x : position.x,
