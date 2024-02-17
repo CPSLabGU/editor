8904b7cb7f66dabaa44b0d252ffdbf8e5da48b22
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2024-02-17 22:35:19 +1000

    Got basic resizing of width and height.

--- src/views/Resizable.tsx
+++ src/views/Resizable.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useCallback, useState, MouseEvent, useEffect,useMemo } from 'react';
 import '../styles/Resizable.css';
 import Point2D from '../models/Point2D';
 
@@ -9,9 +9,109 @@ interface ResizableDimensions {
 }
 
 function Resizable({dimensions, setDimensions, children}) {
-    const [isDragging, setIsDragging] = useState(false);
     const _dimensions = dimensions as ResizableDimensions;
     const _setDimensions = setDimensions as (dimensions: Point2D) => void;
+    const [isDraggingRight, setIsDraggingRight] = useState(false);
+    const [isDraggingLeft, setIsDraggingLeft] = useState(false);
+    const [isDraggingTop, setIsDraggingTop] = useState(false);
+    const [isDraggingBottom, setIsDraggingBottom] = useState(false);
+    const currentDimensions = useMemo(() => (new Point2D(dimensions.dimensions.x, dimensions.dimensions.y)), [dimensions]);
+    const isDragging = useCallback(() => isDraggingRight || isDraggingLeft || isDraggingTop || isDraggingBottom, [isDraggingRight, isDraggingLeft, isDraggingTop, isDraggingBottom]);
+    const mouseDownRight = useCallback((e: MouseEvent) => {
+        e.preventDefault();
+        e.stopPropagation();
+        setIsDraggingRight(true)
+    }, [setIsDraggingRight]);
+    const mouseDownLeft = useCallback((e: MouseEvent) => {
+        e.preventDefault();
+        e.stopPropagation();
+        setIsDraggingLeft(true)
+    }, [setIsDraggingLeft]);
+    const mouseDownTop = useCallback((e: MouseEvent) => {
+        e.preventDefault();
+        e.stopPropagation();
+        setIsDraggingTop(true)
+    }, [setIsDraggingTop]);
+    const mouseDownBottom = useCallback((e: MouseEvent) => {
+        e.preventDefault();
+        e.stopPropagation();
+        setIsDraggingBottom(true)
+    }, [setIsDraggingBottom]);
+    const mouseDownTopRight = useCallback((e: MouseEvent) => {
+        e.preventDefault();
+        e.stopPropagation();
+        setIsDraggingTop(true);
+        setIsDraggingRight(true);
+    }, [setIsDraggingTop, setIsDraggingRight]);
+    const mouseDownTopLeft = useCallback((e: MouseEvent) => {
+        e.preventDefault();
+        e.stopPropagation();
+        setIsDraggingTop(true);
+        setIsDraggingLeft(true);
+    }, [setIsDraggingTop, setIsDraggingLeft]);
+    const mouseDownBottomRight = useCallback((e: MouseEvent) => {
+        e.preventDefault();
+        e.stopPropagation();
+        setIsDraggingBottom(true);
+        setIsDraggingRight(true);
+    }, [setIsDraggingBottom, setIsDraggingRight]);
+    const mouseDownBottomLeft = useCallback((e: MouseEvent) => {
+        e.preventDefault();
+        e.stopPropagation();
+        setIsDraggingBottom(true);
+        setIsDraggingLeft(true);
+    }, [setIsDraggingBottom, setIsDraggingLeft]);
+    useEffect(() => {
+        if (isDragging()) {
+            window.addEventListener;
+        }
+    }, [isDragging]);
+    const move = useCallback((e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        let dx = 0;
+        let dy = 0;
+        let dw = 0;
+        let dh = 0;
+        if (isDraggingRight) {
+            dw += e.movementX;
+        }
+        if (isDraggingLeft) {
+            dw -= e.movementX;
+            dx += e.movementX;
+        }
+        if (isDraggingTop) {
+            dh -= e.movementY;
+            dy += e.movementY;
+        }
+        if (isDraggingBottom) {
+            dh += e.movementY;
+        }
+        const tempW = dw + _dimensions.dimensions.x;
+        const tempH = dh + _dimensions.dimensions.y;
+        const newW = Math.max(Math.min(tempW, _dimensions.maxDimensions.x), _dimensions.minDimensions.x);
+        const newH = Math.max(Math.min(tempH, _dimensions.maxDimensions.y), _dimensions.minDimensions.y);
+        _setDimensions(new Point2D(newW, newH));
+    }, [_setDimensions, isDraggingRight, isDraggingLeft, isDraggingTop, isDraggingBottom, _dimensions]);
+    const endDrag = useCallback(() => {
+        setIsDraggingRight(false);
+        setIsDraggingBottom(false);
+        setIsDraggingLeft(false);
+        setIsDraggingTop(false);
+    }, [setIsDraggingRight, setIsDraggingBottom, setIsDraggingLeft, setIsDraggingTop]);
+    useEffect(() => {
+        if (isDragging()) {
+            window.addEventListener('mousemove', move);
+            window.addEventListener('mouseup', endDrag);
+        } else {
+            window.removeEventListener('mousemove', move);
+            window.removeEventListener('mouseup', endDrag);
+        }
+        return () => {
+            window.removeEventListener('mousemove', move);
+            window.removeEventListener('mouseup', endDrag);
+        };
+    }, [move, isDragging, endDrag]);
     const width = Math.min(
         Math.max(_dimensions.dimensions.x, _dimensions.minDimensions.x, 20),
         _dimensions.maxDimensions.x
@@ -31,16 +131,16 @@ function Resizable({dimensions, setDimensions, children}) {
     return (
         <div>
             <div style={handles} className='handles'>
-                <div className='top-left'></div>
-                <div className='top-middle'></div>
-                <div className='top-right'></div>
+                <div className='top-left' onMouseDown={mouseDownTopLeft}></div>
+                <div className='top-middle' onMouseDown={mouseDownTop}></div>
+                <div className='top-right' onMouseDown={mouseDownTopRight}></div>
                 <div className='clear'></div>
-                <div style={middle} className='left'></div>
-                <div style={middle} className='right'></div>
+                <div style={middle} className='left' onMouseDown={mouseDownLeft}></div>
+                <div style={middle} className='right' onMouseDown={mouseDownRight}></div>
                 <div className='clear'></div>
-                <div className='bottom-left'></div>
-                <div className='bottom-middle'></div>
-                <div className='bottom-right'></div>
+                <div className='bottom-left' onMouseDown={mouseDownBottomLeft}></div>
+                <div className='bottom-middle' onMouseDown={mouseDownBottom}></div>
+                <div className='bottom-right' onMouseDown={mouseDownBottomRight}></div>
             </div>
             <div style={handles} className='resizable-content'>
                 {children}
