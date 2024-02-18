6c5021a64885dcd3422450f9bcf684763f49d385
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2024-02-19 04:25:14 +1000

    Create generic context menu.

---
+++ src/views/ContextMenu.tsx
@@ -0,0 +1,12 @@
+import Point2D from "../models/Point2D"
+import '../styles/ContextMenu.css';
+
+export default function ContextMenu({position, menuItems}: {position: Point2D, menuItems: { [label: string]: () => void }}) {
+  return (
+    <div className="context-menu" style={{top: position.y, left: position.x}}>
+      <ul>
+        {Object.keys(menuItems).map((label) => <li onClick={menuItems[label]}>{label}</li>)}
+      </ul>
+    </div>
+  )
+}
--- src/views/WindowContextMenu.tsx
+++ src/views/WindowContextMenu.tsx
@@ -1,17 +1,9 @@
 import Point2D from "../models/Point2D"
-import "../styles/WindowContextMenu.css"
+import ContextMenu from "./ContextMenu";
 
 function WindowContextMenu({position, createState}: {position: Point2D, createState: () => void}) {
-  const style = {
-    top: position.y,
-    left: position.x
-  }
   return (
-    <div style={style} id="context-menu">
-      <ul>
-        <li onClick={createState}>New State</li>
-      </ul>
-    </div>
+    <ContextMenu position={position} menuItems={{'New State': createState}} />
   )
 }
 
