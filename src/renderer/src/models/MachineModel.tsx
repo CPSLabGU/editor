88e7a87f4f4c437fc1ee6804030ffa66419debc8
Morgan McColl <morgan.mccoll92@gmail.com>
2024-02-20 07:11:56 +1000

    Added IO model.

--- src/renderer/src/models/MachineModel.tsx
+++ src/renderer/src/models/MachineModel.tsx
@@ -1,9 +1,18 @@
+import Point2D from "./Point2D"
+
 export default class MachineModel {
   states: StateModel[]
   externalVariables: string
   machineVariables: string
   includes: string
   transitions: TransitionModel[]
+  constructor(states: StateModel[], externalVariables: string, machineVariables: string, includes: string, transitions: TransitionModel[]) {
+    this.states = states
+    this.externalVariables = externalVariables
+    this.machineVariables = machineVariables
+    this.includes = includes
+    this.transitions = transitions
+  }
 }
 
 export default class StateModel {
@@ -21,4 +30,36 @@ export default class TransitionModel {
   source: string
   target: string
   condition: string
+  constructor(source: string, target: string, condition: string) {
+    this.source = source
+    this.target = target
+    this.condition = condition
+  }
+}
+
+export default class MachineLayout {
+  stateLayouts: StateLayout[]
+  transitionLayouts: TransitionLayout[]
+  constructor(stateLayouts: StateLayout[], transitionLayouts: TransitionLayout[]) {
+    this.stateLayouts = stateLayouts
+    this.transitionLayouts = transitionLayouts
+  }
+}
+
+export default class StateLayout {
+  position: Point2D
+  dimensions: Point2D
+  constructor(position: Point2D, dimensions: Point2D) {
+    this.position = position
+    this.dimensions = dimensions
+  }
+}
+
+export default class TransitionLayout {
+
+  path: BezierPath
+  constructor(path: BezierPath) {
+    this.path = path
+  }
+
 }
