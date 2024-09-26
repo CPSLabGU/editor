73d470dafdc04c151e04f64ce07587442abeac38
Callum <callum.mccoll1@gmail.com>
2024-09-26 12:38:33 +1000

    Add grid background to canvas.

--- renderer/components/canvas/Canvas.tsx
+++ renderer/components/canvas/Canvas.tsx
@@ -204,7 +204,7 @@ export default function Canvas({
     [machine, setMachine]
   )
   return (
-    <div className="bg-background p w-full h-full bg-gradient-to-r from-neutral-500 from-[0.12rem] to-transparent to-[0.12rem] bg-length[2.5rem_2.5rem]" onContextMenu={showContextMenu}>
+    <div className="bg-background w-full h-full bg-" style={{backgroundSize: "4rem 4rem", backgroundImage: "linear-gradient(to right, hsl(var(--secondary)) 1px, transparent 1px), linear-gradient(to bottom, hsl(var(--secondary)) 1px, transparent 1px)"}} onContextMenu={showContextMenu}>
       {Object.keys(machine.transitions).map((id) => {
         const transition = machine.transitions[id]
         const priority = Math.max(
