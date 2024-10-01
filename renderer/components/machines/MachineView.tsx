264fa8a6a5ade489302e961b57fc0d27d72cda16
Callum <callum.mccoll1@gmail.com>
2024-10-02 03:47:54 +1000

    Create verification view.

--- renderer/components/machines/MachineView.tsx
+++ renderer/components/machines/MachineView.tsx
@@ -92,7 +92,7 @@ export default function MachineView({ machine, setMachine, sidePanelHidden }: Ma
         onValueChange={(value) => setMachine(machine.setSelectedTab(value as 'canvas' | 'spec'))}
         className="h-full bg-secondary"
       >
-        <TabsList className="bg-transparent h-8">
+        <TabsList className="h-8">
           <TabsTrigger value="canvas">Editor</TabsTrigger>
           <TabsTrigger value="spec">Verification</TabsTrigger>
         </TabsList>
--- renderer/components/verification/VerificationView.tsx
+++ renderer/components/verification/VerificationView.tsx
@@ -1,12 +1,42 @@
+import CodeEditor from "../code_editor/CodeEditor";
+import GraphView from "../graph/GraphView";
 import Machine from "../machines/Machine";
+import { Button } from "../ui/button";
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "../ui/resizeable";
 
 interface VerificationViewParameters {
-    machine: Machine;
-    setMachine: (newMachine: Machine) => void;
+  machine: Machine;
+  setMachine: (newMachine: Machine) => void;
 }
 
 export default function VerificationView({ machine, setMachine }: VerificationViewParameters): JSX.Element {
-    return (
-        <div>View</div>
-    );
+  return (
+    <div className="w-full h-full">
+      <ResizablePanelGroup direction="vertical">
+        <ResizablePanel>{machine.kripkeStructure && <GraphView svgData={machine.kripkeStructure} />}</ResizablePanel>
+        <ResizableHandle />
+        <ResizablePanel>
+          <div className="p-2">
+            <div className="flex flex-row items-center gap-0 w-full py-2">
+              <div className="flex flex-row items-start gap-1 w-full">
+                <h1>Specification</h1>
+              </div>
+              <div className="flex flex-row items-end gap-1 w-full">
+                <div className="text-right w-full">
+                  <Button>Run</Button>
+                </div>
+              </div>
+            </div>
+            <div>
+              <CodeEditor
+                  language="tctl"
+                  sourcecode={machine.spec}
+                  setSourceCode={(val: string) => setMachine(machine.setSpec(val))}
+              />
+            </div>
+          </div>
+        </ResizablePanel>
+      </ResizablePanelGroup>
+    </div>
+  );
 }
