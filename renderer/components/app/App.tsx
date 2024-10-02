9a0a998fc9c029566f801088e450d29991617b4e
Morgan McColl <morgan.mccoll92@gmail.com>
2024-10-02 11:39:41 +1000

    Update kripke structure from image.

--- renderer/components/app/App.tsx
+++ renderer/components/app/App.tsx
@@ -95,6 +95,10 @@ export default function App(): JSX.Element {
   useEffect(() => {
     window.ipc.didGenerateKripkeStructure((e, path, type, svg) => {
       console.log("Got svg: ", svg)
+      if (svg == '') {
+        setAppState(appState.setKripkeStructure(path, type, undefined))
+        return
+      }
       setAppState(appState.setKripkeStructure(path, type, svg))
     })
   }, [appState, setAppState])
